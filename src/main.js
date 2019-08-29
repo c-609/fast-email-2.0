0461fee01a0a810e7dbd421ba48350ff6d542346
huangan0110 <877347136@qq.com>
2019-08-29 10:50:50 +0800

    去除页面缩放滚动条

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 import Vant from 'vant';
 import 'vant/lib/index.css';
+import  FastClick  from  'fastclick'
+FastClick.attach(document.body);
 Vue.config.productionTip = false
 
 Vue.use(Vant);
